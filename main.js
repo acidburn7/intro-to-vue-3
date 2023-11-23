9dfd62b1c01549d65d866af70d72da2c1d3f37b5
acidburn7 <varaksinaalla@mail.ru>
2023-11-23 15:54:06 +0300

    computed properties

--- main.js
+++ main.js
@@ -7,21 +7,33 @@ const app = Vue.createApp({
                 fontSize: '14px'
             },
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            brand: 'Vue Mastery',
             details: ['50% cotton', '30% wool', '20% polyester'],
+            selectedVariant: 0,
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+              ]
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            console.log(this.selectedVariant)
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
         }
     }
 })
