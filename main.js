075e1d24f0da07d1f48b61ba24d4bac424581582
acidburn7 <varaksinaalla@mail.ru>
2023-11-23 10:53:18 +0300

    list rendering

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ const app = Vue.createApp({
             inStock: false,
             inventory: 2,
             onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+              ]
         }
     }
 })
